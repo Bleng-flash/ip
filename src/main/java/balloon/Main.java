932671e92b3eddec8d51cab55687199c78bfb922
Bryan Leng <bryan.leng33@gmail.com>
2025-09-09 21:42:46 +0800

    Do javafx tutorial part 3 iteration 3

--- src/main/java/balloon/Main.java
+++ src/main/java/balloon/Main.java
@@ -107,8 +107,8 @@ public class Main extends Application {
         String userText = userInput.getText();
         String dukeText = balloon.getResponse(userInput.getText());
         dialogContainer.getChildren().addAll(
-                new DialogBox(userText, userImage),
-                new DialogBox(dukeText, dukeImage)
+                DialogBox.getUserDialog(userText, userImage),
+                DialogBox.getDukeDialog(dukeText, dukeImage)
         );
         userInput.clear();
     }
